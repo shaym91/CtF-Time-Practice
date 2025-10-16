7b6a26198dc3dfcdd728d023039b594bb1538eb8
shaym91 <itsrakibkhan23@gmail.com>
2025-10-16 15:54:17 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -225,4 +225,5 @@ def api_scoreboard():
     } for username, data in leaderboard])
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
